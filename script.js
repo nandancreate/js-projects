5299128066af1ee426df7eaaed92f852b15e72a1
nandancreate <outstry48@gmail.com>
2025-01-25 22:17:03 +0530

    add files

---
+++ script.js
@@ -0,0 +1,15 @@
+const display =document.getElementById("display");
+
+function appendtodisplay(input){
+display.value += input
+}
+
+function cleardisplay(){
+    display.value ="";
+
+}
+
+function calculate(){
+    display.value=eval(display.value)
+
+}
